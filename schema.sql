f30e784a149f5c22d40749f198a32e157d75c9e3
975168367@qq.com <975168367@qq.com>
2017-09-10 21:29:48 +0800

    更新sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
--- drop table if exists entries;
--- create table entries (
---   id integer primary key autoincrement,
---   title string not null,
---   text string not null
--- );
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title string not null,
+  text string not null
+);
 
 drop table if exists comments;
 create table comments (
@@ -15,11 +15,11 @@ create table comments (
   comment string not null
 
 );
--- drop table if exists books;
--- create table books (
---   id integer primary key autoincrement,
---   bookName string not null,
---   bookFileType string not null,
---   savedFile string not NULL
---
--- );
+drop table if exists books;
+create table books (
+  id integer primary key autoincrement,
+  bookName string not null,
+  bookFileType string not null,
+  savedFile string not NULL
+
+);
